ec437862f685c96fe6b9346282e9e9ea474937af
shixing <shixing@tuniu.com>
2015-07-22 10:33:33 +0800

    修改手机端界面

--- js/password.js
+++ js/password.js
@@ -1,28 +1,29 @@
 function countCode() {
-				var password = $("#password").val();
-				var key = $("#key").val()+"@shixing";
-				if (password && key) {
-					var md5one = $.md5(password, key);
-					var md5two = $.md5(md5one, 'raulism');
-					var md5three = $.md5(md5one, 'koniliu');
-					var rule = md5three.split("");
-					var source = md5two.split("");
-					for (var i = 0; i <= 31; i++) {
-						if (isNaN(source[i])) {
-							str = "raulismlovekoniliuatNanjing20140302";
-							if (str.search(rule[i]) > -1) {
-								source[i] = source[i].toUpperCase();
-							}
-						}
-					}
-					var code32 = source.join("");
-					var code1 = code32.slice(0, 1);
-					if (isNaN(code1)) {
-						var code16 = code32.slice(0, 16);
-					} else {
-						var code16 = code32.slice(1, 16) + "N";
-					}
-					$("#code16").text(code16);
-					document.getElementById('code16').value = code16; 
+	var password = $("#password").val();
+	var key = $("#key").val()+"@shixing";
+	if (password && key) {
+		var md5one = $.md5(password, key);
+		var md5two = $.md5(md5one, 'raulism');
+		var md5three = $.md5(md5one, 'koniliu');
+		var rule = md5three.split("");
+		var source = md5two.split("");
+		for (var i = 0; i <= 31; i++) {
+			if (isNaN(source[i])) {
+				str = "raulismlovekoniliuatNanjing20140302";
+				if (str.search(rule[i]) > -1) {
+					source[i] = source[i].toUpperCase();
 				}
 			}
+		}
+		var code32 = source.join("");
+		var code1 = code32.slice(0, 1);
+		if (isNaN(code1)) {
+			var code16 = code32.slice(0, 16);
+		} else {
+			var code16 = code32.slice(1, 16) + "N";
+		}
+		$("#code16").val(code16);
+		//document.getElementById('code16').value = code16; 
+		$('#copyPassword').val(code16);
+	}
+}
